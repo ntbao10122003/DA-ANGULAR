6d265c354d4fc05f08aa4417007f5278d1ca7230
huy <havu6544@gmail.com>
2023-06-06 21:17:12 +0700

    feat: AA-33-fe-be-creat-product refs: AA-33

--- src/app/page/admin/product-add/product-add.component.ts
+++ src/app/page/admin/product-add/product-add.component.ts
@@ -4,6 +4,7 @@ import { IProduct } from 'src/app/interfaces/product';
 import { ProductService } from 'src/app/services/services.component';
 import { HttpClient } from '@angular/common/http';
 import { UploadServiceService } from 'src/app/services/upload-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-add',
@@ -15,16 +16,20 @@ export class ProductAddComponent {
     private productService: ProductService,
     private formBuilder: FormBuilder,
     private http: HttpClient,
-    private uploadService: UploadServiceService
+    private uploadService: UploadServiceService,
+    private router: Router
   ) { }
-
+  
+  categories: any[] = [];
   files: File[] = [];
   productForm = this.formBuilder.group({
     name: [''],
     price: [0],
-    description: [''],
+    shortDesc: [''],
+    brand: [''],
     categoryId: [''],
-    img: ['']
+    images: [''],
+    longDesc: [''],
   });
 
   async uploadFiles(files: File[]) {
@@ -51,23 +56,31 @@ export class ProductAddComponent {
   async onHandleAdd() {
     if (this.productForm.valid) {
       const product: IProduct = {
-        name: this.productForm.value.name || '',
+        // _id: this.productForm._id,
+        name: this.productForm.value.name || "",
         price: this.productForm.value.price || 0,
-        description: this.productForm.value.description || '',
-        categoryId: this.productForm.value.categoryId || '',
-        img: this.productForm.value.img || '',
+        shortDesc: this.productForm.value.shortDesc || "",
+        brand: this.productForm.value.brand || "",
+        categoryId: this.productForm.value.categoryId || "",
+        images: this.productForm.value.images || "",
+        longDesc: this.productForm.value.longDesc || "",
       };
       const uploadedImageUrl = await this.uploadFiles(this.files);
       if (uploadedImageUrl) {
-        product.img = uploadedImageUrl;
+        product.images = uploadedImageUrl;
       }
       
       this.productService.addProduct(product).subscribe((product) => {
         console.log('Thành công', product);
+        this.router.navigate(['/admin']);
       });
     }
   }
-  
+  ngOnInit() {
+    this.productService.getCategories().subscribe((categories) => {
+      this.categories = categories;
+    });
+  }
   onFileChange(event: any) {
     this.files = event.target.files;
   }
--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IProduct } from '../interfaces/product';
+import { ICategories } from '../interfaces/category';
 
 @Injectable({
  providedIn : 'root'
@@ -12,24 +13,35 @@ export class ProductService {
   constructor(private http : HttpClient){}
 
   getProducts ():Observable<any> {
-    return this.http.get<IProduct[]> (`http://localhost:8082/api/products`)
+    return this.http.get<IProduct[]> (`http://localhost:8080/api/products`)
   }
-
   getProductById (id :any):Observable<IProduct> {
-    return this.http.get<IProduct> (`http://localhost:8082/api/products/${id}`)
+    return this.http.get<IProduct> (`http://localhost:8080/api/products/${id}`)
   }
-
-
   addProduct (product : IProduct):Observable<IProduct> {
-    return this.http.post<IProduct> (`http://localhost:8082/api/products`,product)
+    return this.http.post<IProduct> (`http://localhost:8080/api/products`,product)
   }
-
-
   updateProduct (product : IProduct):Observable<IProduct> {
-    return this.http.patch<IProduct> (`http://localhost:8082/api/products/${product._id}`,product)
+    return this.http.patch<IProduct> (`http://localhost:8080/api/products/${product._id}`,product)
   }
-
   deleteProduct (id:any):Observable<IProduct> {
-    return this.http.delete<IProduct> (`http://localhost:8082/api/products/${id}`)
+    return this.http.delete<IProduct> (`http://localhost:8080/api/products/${id}`)
+  }
+  
+
+  getCategories ():Observable<any> {
+    return this.http.get<ICategories[]> (`http://localhost:8080/api/categories`)
+  }
+  getCategoryById (id: any):Observable<any> {
+    return this.http.get<ICategories[]> (`http://localhost:8080/api/categories/${id}`)
+  }
+  addCategory (product: ICategories):Observable<any> {
+    return this.http.post<ICategories[]> (`http://localhost:8080/api/categories`, product)
+  }
+  updateCategory (product : ICategories):Observable<IProduct> {
+    return this.http.patch<IProduct> (`http://localhost:8080/api/categories/${product._id}`,product)
+  }
+  deleteCategory (id:any):Observable<IProduct> {
+    return this.http.delete<IProduct> (`http://localhost:8080/api/categories/${id}`)
   }
 }

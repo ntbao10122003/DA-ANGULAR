9cd9e4d14eed8896c4192ac86cfe12b69bbe43ff
nguyenhoainam <nguyenhoaiinam2003@gmail.com>
2023-06-12 23:01:06 +0700

    fix edit product

--- src/app/page/admin/product-edit/product-edit.component.ts
+++ src/app/page/admin/product-edit/product-edit.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { IProduct } from 'src/app/interfaces/product';
 import { ProductService } from 'src/app/services/services.component';
 import { FormBuilder, Validators } from '@angular/forms';
+import { UploadServiceService } from 'src/app/services/upload-service.service';
 @Component({
   selector: 'app-product-edit',
   templateUrl: './product-edit.component.html',
@@ -11,6 +12,7 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class ProductEditComponent {
   product!: IProduct;
   categories: any[] = [];
+  files: File[] = [];
   productForm = this.formBuilder.group({
     name: [''],
     price: [0],
@@ -25,7 +27,8 @@ export class ProductEditComponent {
     private productService: ProductService,
     private route: ActivatedRoute,
     private formBuilder: FormBuilder,
-    private router: Router) {
+    private router: Router,
+    private uploadService: UploadServiceService) {
     this.route.paramMap.subscribe(param => {
       const id = String(param.get('id'));
       this.productService.getProductById(id).subscribe(product => {
@@ -42,7 +45,27 @@ export class ProductEditComponent {
       }, error => console.log(error.message))
     })
   }
-  onHandleUpdate() {
+  async uploadFiles(files: File[]) {
+    if (files && files.length > 0) {
+      const formData = new FormData();
+      for (const file of files) {
+        formData.append('file', file);
+      }
+      formData.append('upload_preset', 'ecma-we17303');
+      formData.append('cloud_name', 'dds5d6hm0');
+  
+      const uploadedFiles = await this.uploadService.uploadImage(formData).toPromise();
+      console.log(uploadedFiles);
+      
+      // Xử lý URL trả về từ Cloudinary
+      if (uploadedFiles && uploadedFiles.url) {
+        const imageUrl = uploadedFiles.url;
+        console.log(imageUrl);
+        return imageUrl;
+      }
+    }
+  }
+  async onHandleUpdate() {
     // kiểm tra nếu form hợp lệ 
     if (this.productForm.valid) {
       const newProduct: IProduct = {
@@ -56,6 +79,10 @@ export class ProductEditComponent {
         longDesc: this.productForm.value.longDesc || this.product.longDesc,
        
       }
+      const uploadedImageUrl = await this.uploadFiles(this.files);
+      if (uploadedImageUrl) {
+        newProduct.images = uploadedImageUrl;
+      }
       this.productService.updateProduct(newProduct).subscribe(product => {
         console.log(product);
         this.router.navigate(['/admin']);
@@ -68,4 +95,7 @@ export class ProductEditComponent {
     });
   }
   
+  onFileChange(event: any) {
+    this.files = event.target.files;
+  }
 }

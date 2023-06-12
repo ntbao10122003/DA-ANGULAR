35657ee0bff3bedfe98c2dec0c33217953f4594e
nguyenhoainam <nguyenhoaiinam2003@gmail.com>
2023-06-12 22:26:23 +0700

    fix add to cart

--- src/app/page/cart/cart.component.ts
+++ src/app/page/cart/cart.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CartService } from '../../services/cart.service';
 import { ICart, ICartItem } from '../../interfaces/cart';
+import { FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-cart',
@@ -10,21 +12,46 @@ import { ICart, ICartItem } from '../../interfaces/cart';
 })
 export class CartComponent implements OnInit {
   cartInfo: ICartItem[] = [];
+  totalAmount: number = 0;
   id!: string | null;
-
-  constructor(private route: ActivatedRoute, private cartService: CartService) { }
+  cartForm = this.formBuilder.group({
+    name: [''],
+    email: [''],
+    phone: [],
+    address: ['']
+  })
+  
+  constructor(private route: ActivatedRoute, private cartService: CartService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       this.id = params.get('id');
       console.log(this.id);
 
+      const localStorageData = localStorage.getItem('accessToken');
+      if (localStorageData) {
+        const data = JSON.parse(localStorageData);
+        this.cartForm.patchValue({
+          name: data.auth.name,
+          email: data.auth.email,
+          phone: data.auth.phone
+        })
+      }
+
       if (this.id) {
         this.cartService.getOneCart(this.id).subscribe(res => {
           this.cartInfo = res.items;
           console.log(this.cartInfo);
+          this.calculateTotalAmount()
         });
       }
     });
+    
+  }
+  calculateTotalAmount(): void {
+    this.totalAmount = 0;
+    for (let cartItem of this.cartInfo) {
+      this.totalAmount += cartItem.price * cartItem.quantity;
+    }
   }
 }
